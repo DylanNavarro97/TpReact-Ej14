e6940e8718173c2be54d7f392bdc425f73b728a4
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-02-23 21:24:37 -0300

    header y footer basicos agregados

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Menu from './components/common/Menu';
+import Inicio from './components/pages/Inicio';
+import Footer from './components/common/Footer';
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route exact path="/" element={<Inicio />}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
