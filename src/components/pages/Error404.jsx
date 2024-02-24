e6184cabeb5af0598f71c8b23fcd975c8dbc9782
Elias Juarez <juarezelias83@gmail.com>
2024-02-24 19:56:18 -0300

    se creo la rama error404 junto con el componente Error404.jsx, y se agrego la imagen

---
+++ src/components/pages/Error404.jsx
@@ -0,0 +1,11 @@
+import error from "../../assets/error404.png"
+
+const Error404 = () => {
+    return (
+        <section className="container">
+            <img src="" alt="" />
+        </section>
+    );
+};
+
+export default Error404;
