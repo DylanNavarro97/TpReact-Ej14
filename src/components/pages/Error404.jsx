6da14c097b5e4233d2b6905740a86856e9ca53a6
Elias Juarez <juarezelias83@gmail.com>
2024-02-24 19:58:57 -0300

    se agrego el boton de react-bootstrap

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,9 +1,13 @@
 import error from "../../assets/error404.png"
+import { Button } from "react-bootstrap";
 
 const Error404 = () => {
     return (
         <section className="container">
-            <img src="" alt="" />
+            <img src={error} alt="imagen de error 404" />
+            <div>
+                <Button variant="primary">Volver al inicio</Button>
+            </div>
         </section>
     );
 };
