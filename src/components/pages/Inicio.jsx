9b927df2de2444cc99cd357718a9e4aeb9a3827b
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-02-24 12:40:10 -0300

    Container con grid flex agregado a inicio. Esta aclarado donde hay que hacer map de cards

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import {Card, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 
 function Inicio() {
   return (
-    <section className='mainSection'>
-
+    <section className="mainSection">
+      <Container fluid className="my-4">
+        <div className="row">
+          <div className="col-md-4 col-lg-2 mb-3">
+            {/* Aca va el map de las cards */}
+          </div>
+        </div>
+      </Container>
     </section>
-  )
+  );
 }
 
-export default Inicio
+export default Inicio;
