4bfb25b9092204603e9070e9693c839433d3f9ca
Nano <luxorius07@gmail.com>
2024-02-26 11:37:01 -0300

    se creo el formulario crear y se dejo el rout comentado hasta tener la pagina administrador

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Menu from './components/common/Menu';
 import Inicio from './components/pages/Inicio';
 import Footer from './components/common/Footer';
+import FormularioRecetas from './components/recetas/FormularioRecetas';
 
 function App() {
 
@@ -13,9 +14,11 @@ function App() {
         <Menu />
         <Routes>
           <Route exact path="/" element={<Inicio />}/>
+          {/* <Route exact path="/" element={<FormularioRecetas/>}></Route> */}
         </Routes>
         <Footer />
       </BrowserRouter>
+      <FormularioRecetas/>
     </>
   )
 }
---
+++ src/components/recetas/FormularioRecetas.jsx
@@ -0,0 +1,76 @@
+import { Form, Button } from "react-bootstrap";
+
+
+const FormularioRecetas = () => {
+
+  return (
+    <section className="container mainSection">
+      <h1 className="display-4 mt-5">Nueva Receta</h1>
+      <hr />
+      <Form className="my-4">
+        <Form.Group className="mb-3" controlId="formNombreReceta">
+          <Form.Label>Receta*</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Escriba aquí el nombre de su receta"
+          />
+          <Form.Text className="text-danger">
+            prueba de error
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formImagen">
+          <Form.Label>Imagen URL*</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ej: https://www.pexels.com/es-es/algunaFotoDelPlatoTerminado/"
+          />
+          <Form.Text className="text-danger">
+        prueba de error
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formPrecio">
+          <Form.Label>Categoría*</Form.Label>
+          <Form.Select>
+            <option value="">Seleccione una opcion</option>
+            <option value="Infusiones">Sopas</option>
+            <option value="Batidos">Carnes</option>
+            <option value="dulce">Pastas</option>
+            <option value="salado">Salsas</option>
+            <option value="salado">Ensaladas</option>
+          </Form.Select>
+          <Form.Text className="text-danger">
+          prueba de error
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formImagen">
+          <Form.Label>Descripción breve*</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ej: Vacío a la plancha."
+            as="textarea"
+          />
+          <Form.Text className="text-danger">
+        prueba de error
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formImagen">
+          <Form.Label>Descripción Amplia*</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ej: El vacio a la plancha es un plato clásico de la cocina argentina que destaca por su sencillez y delicioso sabor. Consiste en una pieza de carne de vacuno, típicamente vacío (también conocido como matambre), que se cocina a la plancha."
+            as="textarea"
+          />
+          <Form.Text className="text-danger">
+        prueba de error
+          </Form.Text>
+        </Form.Group>
+        
+        <Button type="submit" variant='success'>
+          Guardar
+        </Button>
+      </Form>
+    </section>
+  );
+};
+
+export default FormularioRecetas;
