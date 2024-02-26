c52af3822a5efd818a2e95b8d7c7d8e2ced88a70
Elias Juarez <juarezelias83@gmail.com>
2024-02-26 05:08:20 -0300

    se modifico la imagen y se dio clases de bootstrap para centrar la imagen, los textos y el boton

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -3,9 +3,11 @@ import { Button } from "react-bootstrap";
 
 const Error404 = () => {
     return (
-        <section className="container">
-            <img src={error} alt="imagen de error 404" />
-            <div>
+        <section className="container w-100 d-flex flex-column align-items-center text-center">
+            <img src={error} alt="imagen de error 404" className="w-50 mt-4" />
+            <h3>Ooops... Error 404</h3>
+            <h4 className="my-4">Lo sentimos, no se pudo encontrar la pagina</h4>
+            <div className="mb-4">
                 <Button variant="primary">Volver al inicio</Button>
             </div>
         </section>
