2ca2539a6953dcbb699027c36dd7de9ef0bd00cd
Elias Juarez <juarezelias83@gmail.com>
2024-02-27 19:10:00 -0300

    se creo la funcion borrarReceta y se agrego la alerta de Sweetalert

--- src/components/recetas/ItemReceta.jsx
+++ src/components/recetas/ItemReceta.jsx
@@ -1,7 +1,30 @@
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ItemReceta = ({ receta }) => {
+
+  const borrarReceta = () =>{
+    Swal.fire({
+      title: "¿Estas seguro de eliminar la receta?",
+      text: "No se puede revertir este proceso",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrar",
+      cancelButtonText: "Cancelar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Receta eliminada",
+          text: `La Receta "${receta.nombre}" ha sido eliminada correctamente.`,
+          icon: "success"
+        });
+      }
+    });
+  }
+
   return (
     <tr>
       <td className="text-center">{receta.id}</td>
@@ -19,7 +42,7 @@ const ItemReceta = ({ receta }) => {
         <Link to={`/administrador/editar/${receta.id}`} variant="warning" className="me-lg-2 btn btn-warning">
         <i className="bi bi-pencil-square"></i>
         </Link>
-        <Button variant="danger">
+        <Button variant="danger" onClick={borrarReceta}>
         <i className="bi bi-trash"></i>
         </Button>
       </td>
