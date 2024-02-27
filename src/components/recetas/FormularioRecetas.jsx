4e6254eafdc6909dc111d66ef5b1de2d93c43c38
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-02-26 21:26:19 -0300

    validacion de nombre, imagen y categoria en formulario receta

--- src/components/recetas/FormularioRecetas.jsx
+++ src/components/recetas/FormularioRecetas.jsx
@@ -1,45 +1,74 @@
 import { Form, Button } from "react-bootstrap";
-
+import { useForm } from "react-hook-form";
 
 const FormularioRecetas = () => {
-
   const {
     register,
     handleSubmit,
-    formState: {errors},
-    reset, 
-    setValue
-  } = useForm()
+    formState: { errors },
+    reset,
+    setValue,
+  } = useForm();
+
+  const recetaValidada = (receta) => {
+    console.log(receta)
+  };
 
   return (
     <section className="container mainSection">
       <h1 className="display-4 mt-5">Nueva Receta</h1>
       <hr />
-      <Form className="my-4">
+      <Form className="my-4" onSubmit={handleSubmit(recetaValidada)}>
         <Form.Group className="mb-3" controlId="formNombreReceta">
           <Form.Label>Receta*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Escriba aquí el nombre de su receta"
+            {...register("nombre", {
+              required: "El nombre de la receta es obligatoria",
+              minLength: {
+                value: 2,
+                message:
+                  "El nombre de la receta debe tener como mínimo 2 caracteres",
+              },
+              maxLength: {
+                value: 30,
+                message:
+                  "El nombre de la receta debe tener como maximo 30 caracteres",
+              },
+            })}
+            maxLength={30}
           />
           <Form.Text className="text-danger">
-            prueba de error
+            {errors.nombre?.message}
           </Form.Text>
         </Form.Group>
+
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Imagen URL*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: https://www.pexels.com/es-es/algunaFotoDelPlatoTerminado/"
+            {...register("imagen", {
+              required: "La imagen es obligatoria",
+              pattern: {
+                value: /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/,
+                message: "Debe ingresar una URL valida (jpg|jpeg|gif|png)"
+              }
+            })}
           />
           <Form.Text className="text-danger">
-        prueba de error
+            {errors.imagen?.message}
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Categoría*</Form.Label>
-          <Form.Select>
-            <option>Seleccione una opción</option>
+          <Form.Select
+          {...register ("categoria", {
+            required: "La categoria es obligatoria"
+          })}
+          >
+            <option value="">Seleccione una opción</option>
             <option value="Sopas">Sopas</option>
             <option value="Carnes">Carnes</option>
             <option value="Pastas">Pastas</option>
@@ -47,7 +76,7 @@ const FormularioRecetas = () => {
             <option value="Ensaladas">Ensaladas</option>
           </Form.Select>
           <Form.Text className="text-danger">
-          prueba de error
+            {errors.categoria?.message}
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
@@ -57,9 +86,7 @@ const FormularioRecetas = () => {
             placeholder="Ej: Vacío a la plancha."
             as="textarea"
           />
-          <Form.Text className="text-danger">
-        prueba de error
-          </Form.Text>
+          <Form.Text className="text-danger">prueba de error</Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Descripción Amplia*</Form.Label>
@@ -68,12 +95,10 @@ const FormularioRecetas = () => {
             placeholder="Ej: El vacio a la plancha es un plato clásico de la cocina argentina que destaca por su sencillez y delicioso sabor. Consiste en una pieza de carne de vacuno, típicamente vacío (también conocido como matambre), que se cocina a la plancha."
             as="textarea"
           />
-          <Form.Text className="text-danger">
-        prueba de error
-          </Form.Text>
+          <Form.Text className="text-danger">prueba de error</Form.Text>
         </Form.Group>
-        
-        <Button type="submit" variant='success'>
+
+        <Button type="submit" variant="success">
           Guardar
         </Button>
       </Form>
