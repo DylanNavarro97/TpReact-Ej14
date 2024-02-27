5dd797c58be8d536603805e8dbc6d4338a11e8b0
Elias Juarez <juarezelias83@gmail.com>
2024-02-27 04:16:43 -0300

    se hizo el map de recetas en Admin.jsx y se utilizo el props receta para completar la tabla con cada receta

--- src/components/recetas/ItemReceta.jsx
+++ src/components/recetas/ItemReceta.jsx
@@ -1,15 +1,18 @@
 import { Button } from "react-bootstrap";
 
-const ItemReceta = () => {
+const ItemReceta = ({ receta }) => {
   return (
     <tr>
-      <td className="text-center"></td>
-      <td></td>
-      <td className="text-end"></td>
+      <td className="text-center">{receta.id}</td>
+      <td>{receta.nombre}</td>
       <td className="text-center">
-        <img src="" alt="" />
+        <img
+          src={receta.imagen}
+          alt={receta.nombre}
+        />
       </td>
-      <td></td>
+      <td className="text-center">{receta.categoria}</td>
+      <td>{receta.nombre}</td>
       <td className="text-center">
         <Button variant="warning" className="me-lg-2"></Button>
         <Button variant="danger"></Button>
